42ece55d2d10985521ecd02078f925900b1738df
John Hodson <hodsonus@gmail.com>
2022-05-07 13:22:22 -0700

    cleaned up code, working as expected.

--- sleeper_roster_extractor.py
+++ sleeper_roster_extractor.py
@@ -1,14 +1,91 @@
 import json
-from xml.dom import NotFoundErr
+import os
 import pandas as pd
 from requests import get
 
+
 base_url = 'https://api.sleeper.app/v1/'
-league_id = '784488936675385344'
+league_id = '000000000000000000'
 league_url = base_url + 'league/' + str(league_id) + '/'
 
-def add_player_to_roster(rosters_dict, roster_id, player):
-    rosters_dict[roster_id] += [player]
+
+def main():
+
+    rosters = get(league_url + 'rosters').json()
+    ownerid_to_playerids = get_ownerid_to_playerids(rosters)
+
+    owners = get(league_url + 'users').json()
+    ownerid_to_ownerdisplaynames = get_ownerid_to_ownerdisplaynames(owners)
+
+    players = get(base_url + 'players/nfl').json()
+
+    ownerdisplayname_to_playerattributelists, playerattributelist_columns = resolve_ids_to_names_and_attributelists(ownerid_to_playerids, ownerid_to_ownerdisplaynames, players)
+        
+    os.makedirs("./rosters/", exist_ok=True)
+
+    for ownerdisplayname in ownerdisplayname_to_playerattributelists:
+        roster_df = pd.DataFrame(ownerdisplayname_to_playerattributelists[ownerdisplayname], columns = playerattributelist_columns)
+        file_name = './rosters/' + str(ownerdisplayname) + ".csv"
+        roster_df.to_csv(file_name, encoding='utf-8', index=False)
+
+
+def get_ownerid_to_ownerdisplaynames(owners):
+    ownerid_to_ownerdisplaynames = {}
+    for owner in owners:
+        ownerid_to_ownerdisplaynames[owner["user_id"]] = owner["display_name"]
+    return ownerid_to_ownerdisplaynames
+
+
+def resolve_ids_to_names_and_attributelists(ownerid_to_playerids, ownerid_to_ownerdisplaynames, players):
+    ownerdisplayname_to_playerattributelists = {}
+
+    for ownerid in ownerid_to_playerids:
+
+        ownerdisplayname = ownerid_to_ownerdisplaynames[ownerid]
+        ownerdisplayname_to_playerattributelists[ownerdisplayname] = []
+
+        for player_id in ownerid_to_playerids[ownerid]:
+
+            player = players[player_id]
+
+            full_name = player["full_name"]
+            age = player["age"]
+            years_exp = player["years_exp"]
+            height = player["height"]
+            weight = player["weight"]
+            college = player["college"]
+            fantasy_positions = ",".join(player["fantasy_positions"])
+
+            ownerdisplayname_to_playerattributelists[ownerdisplayname] += [[full_name, age, years_exp, fantasy_positions, height, weight, college]]
+    
+    playerattributelist_columns = ['Name', 'Age', "YoE", "Eligibility", "Height", "Weight", "College"]
+    return ownerdisplayname_to_playerattributelists, playerattributelist_columns
+
+
+def get_ownerid_to_playerids(rosters):
+    ownerid_to_playerids = {}
+    for roster in rosters:
+
+        owner_id = roster["owner_id"]
+        ownerid_to_playerids[owner_id] = []
+    
+        for taxi_playerid in roster["taxi"]:
+            if not is_int(taxi_playerid):
+                continue
+            ownerid_to_playerids[owner_id] += [taxi_playerid]
+
+        for starter_playerid in roster["starters"]:
+            if not is_int(starter_playerid):
+                continue
+            ownerid_to_playerids[owner_id] += [starter_playerid]
+
+        for bench_playerid in roster["players"]:
+            if not is_int(bench_playerid):
+                continue
+            ownerid_to_playerids[owner_id] += [bench_playerid]
+
+    return ownerid_to_playerids
+
 
 def is_int(s):
     try: 
@@ -17,129 +94,6 @@ def is_int(s):
     except ValueError: 
         return False
 
-def resolve_rosters_dict(players, users, rosters_dict):
-    resolved_rosters_dict = {}
-    for user_id in rosters_dict:
 
-        user_displayname = None
-        for user in users:
-            if user["user_id"] == user_id:
-                user_displayname = user["display_name"]
-                break
-        if (user_displayname == None):
-            raise NotFoundErr
-
-        resolved_rosters_dict[user_displayname] = []
-
-        for player_id in rosters_dict[user_id]:
-
-            player = players[player_id]
-            print(player["full_name"])
-            resolved_rosters_dict[user_displayname] += [player]
-    
-    return resolved_rosters_dict
- 
-rosters = get(league_url + 'rosters').json()
-rosters_dict = {}
-
-for roster in rosters:
-
-    roster_id = roster["owner_id"]
-    rosters_dict[roster_id] = []
-  
-    for taxi_player in roster["taxi"]:
-        add_player_to_roster(rosters_dict, roster_id, taxi_player)
-
-    for starter_id in roster["starters"]:
-        if not is_int(starter_id):
-            continue
-        add_player_to_roster(rosters_dict, roster_id, starter_id)
-
-    for player in roster["players"]:
-        if not is_int(player):
-            continue
-
-        add_player_to_roster(rosters_dict, roster_id, player)
-
-players = get(base_url + 'players/nfl').json()
-users = get(league_url + 'users').json()
-
-resolved_rosters_dict = resolve_rosters_dict(players, users, rosters_dict)
-
-roster_lists = {}
-
-for roster_id in resolved_rosters_dict:
-    roster_lists[roster_id] = []
-
-    # {
-    #     "2103": {
-    #       "injury_body_part": null,
-    #       "rotoworld_id": null,
-    #       "full_name": "Cody Booth",
-    #       "metadata": null,
-    #       "fantasy_positions": [
-    #         "OL"
-    #       ],
-    #       "sportradar_id": "4cd4976e-e230-4935-ad3f-c12876a41350",
-    #       "news_updated": null,
-    #       "search_rank": 9999999,
-    #       "search_first_name": "cody",
-    #       "fantasy_data_id": 16426,
-    #       "injury_status": null,
-    #       "stats_id": null,
-    #       "weight": "285",
-    #       "player_id": "2103",
-    #       "first_name": "Cody",
-    #       "status": "Inactive",
-    #       "pandascore_id": null,
-    #       "depth_chart_order": null,
-    #       "college": "Temple",
-    #       "practice_participation": null,
-    #       "years_exp": 1,
-    #       "position": "OT",
-    #       "high_school": null,
-    #       "birth_state": null,
-    #       "rotowire_id": 9866,
-    #       "team": null,
-    #       "number": 60,
-    #       "espn_id": 17054,
-    #       "gsis_id": null,
-    #       "birth_city": null,
-    #       "birth_date": "1991-04-22",
-    #       "yahoo_id": 27841,
-    #       "height": "6'5\"",
-    #       "last_name": "Booth",
-    #       "age": 27,
-    #       "swish_id": null,
-    #       "depth_chart_position": null,
-    #       "injury_notes": null,
-    #       "active": false,
-    #       "birth_country": null,
-    #       "injury_start_date": null,
-    #       "practice_description": null,
-    #       "sport": "nfl",
-    #       "hashtag": "#codybooth-NFL-FA-60",
-    #       "search_full_name": "codybooth",
-    #       "search_last_name": "booth"
-    #     }
-    #   }
-    for player_object in resolved_rosters_dict[roster_id]:
-        full_name = player_object["full_name"]
-
-        age = player_object["age"]
-        years_exp = player_object["years_exp"]
-
-        height = player_object["height"]
-        weight = player_object["weight"]
-
-        depth = player_object["depth_chart_position"]
-        college = player_object["college"]
-
-        fantasy_positions = ",".join(player_object["fantasy_positions"])
-
-        roster_lists[roster_id] += [[full_name, age, years_exp, fantasy_positions, height, weight, depth]]
-
-for roster_id in roster_lists:
-    roster_df = pd.DataFrame(roster_lists[roster_id], columns = ['Name', 'Age', "YoE", "Eligibility", "Height", "Weight", "Depth"])
-    file_name = str(roster_id) + ".csv"
-    roster_df.to_csv(file_name, encoding='utf-8', index=False)
+if __name__ == "__main__":
+    main()
